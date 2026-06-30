03d57c437a95b95c99dcbfb75b91f3070ded1249
zacube <contact@zacube.com>
2026-06-30 09:02:15 +0200

    feat: Ajoute sur User `PasswordAuthenticatedUserInterface` + attribut ORM sur `$password`. Met à jour la bd avec `make:migration` + `doctrine:migrations:migrate`

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User implements UserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -26,10 +27,7 @@ class User implements UserInterface
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    /**
-     * @var string|null The hashed password
-     */
-
+    #[ORM\Column]
     private ?string $password = null;
 
     #[ORM\Column(type: 'text', nullable: true)]
@@ -144,8 +142,14 @@ class User implements UserInterface
      */
     public function getPassword(): ?string
     {
-/*        return $this->password;*/
-        return null;
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
     }
 
     /**
