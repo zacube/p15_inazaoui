a496e88a9e27c3c0435a86d1315882cdac21fddb
zacube <contact@zacube.com>
2026-06-30 19:27:48 +0200

    feat: mise à jour de UserFixtures.php avec le hashage des mdp

--- src/Fixtures/UserFixtures.php
+++ src/Fixtures/UserFixtures.php
@@ -5,26 +5,35 @@ namespace App\Fixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class UserFixtures extends Fixture
 {
     public const ADMIN_REFERENCE = 'user-admin';
     public const USER_REFERENCE_PREFIX = 'user-invite-';
     public const NB_INVITES = 5;
+
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+    )
+    {}
+
     public function load(ObjectManager $manager): void
     {
-        $admin = (new User())
-            ->setName('Ina Zaoui')
+        $admin = new User();
+        $admin->setName('Ina Zaoui')
             ->setEmail('ina@zaoui.com')
-            ->setAdmin(true);
+            ->setAdmin(true)
+            ->setPassword($this->passwordHasher->hashPassword($admin, 'password'));
         $manager->persist($admin);
         $this->addReference(self::ADMIN_REFERENCE, $admin);
 
         for ($i = 1; $i <= 5; $i++) {
-            $guest = (new User())
-                ->setName('Invité ' . $i)
+            $guest = new User();
+            $guest->setName('Invité ' . $i)
                 ->setEmail('invite+' . $i . '@example.com')
-                ->setAdmin(false);
+                ->setAdmin(false)
+                ->setPassword($this->passwordHasher->hashPassword($guest, 'aze'));
             $manager->persist($guest);
             $this->addReference(self::USER_REFERENCE_PREFIX . $i, $guest);
         }
